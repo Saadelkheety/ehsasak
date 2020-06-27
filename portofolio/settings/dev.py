7ad211c7bbe9d65d5320e3784297671785ae15c1
Saad Elkheety <saadelkheety@gmail.com>
2020-06-27 07:32:54 +0200

    disable debug

--- portofolio/settings/dev.py
+++ portofolio/settings/dev.py
@@ -2,7 +2,7 @@ from .base import *
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'great secret key')
